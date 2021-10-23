eaa5321af999f20f55df3d276f7b3644fc66c37e
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-23 09:33:53 -0400

    Add course, ticket models

--- models/course.ts
+++ models/course.ts
@@ -1,6 +1,14 @@
+interface Course {
+  id: number;
+  number: string;
+  name: string;
+}
+
 export const courses = [
   { id: 0, number: "COP 1000C", name: "Intro to Programming Concepts" },
   { id: 1, number: "COP 2800C", name: "Java Programming I" },
   { id: 2, number: "HUM 1020", name: "Intro to Humanities" },
   { id: 3, number: "MAC 1140", name: "Precalculus Algebra" },
 ];
+
+export default Course;
