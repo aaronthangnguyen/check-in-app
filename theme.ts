0c2d96f13c734390349c884785eea195107c37b5
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-16 10:14:24 -0500

    Create first commit

---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "IBM Plex Serif",
+    body: "IBM Plex Sans",
+  },
+});
+
+export default theme;
