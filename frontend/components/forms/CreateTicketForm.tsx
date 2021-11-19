bc8f529ab2324c44ba1f9524048d90eed422838d
Aaron Nguyen <me@aaronnguyen.dev>
2021-11-18 22:19:24 -0500

    Implement check-in form

---
+++ frontend/components/forms/CreateTicketForm.tsx
@@ -0,0 +1,118 @@
+import { CheckIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Select,
+} from "@chakra-ui/react";
+import axios, { Method } from "axios";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { KeyedMutator } from "swr";
+import { CreateTicket, Ticket } from "../../types/ticket";
+
+interface CreateTicketFormProps {
+  onMutate: KeyedMutator<Ticket[]>;
+}
+
+export const CreateTicketForm = ({ onMutate }: CreateTicketFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setFocus,
+    formState: { errors, isSubmitting },
+  } = useForm();
+  const options = getOptions();
+
+  const onSubmit: SubmitHandler<CreateTicket> = async (ticket) => {
+    reset();
+    setFocus("studentId");
+    await axios
+      .request({
+        method: "POST" as Method,
+        url: `http://localhost:3000/tickets`,
+        headers: { "Content-Type": "application/json" },
+        data: ticket,
+      })
+      .then((res) => {})
+      .catch((err) => {
+        console.error(err);
+      });
+    onMutate();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Student ID */}
+      <FormControl isInvalid={errors.studentId}>
+        <FormLabel id="student-id-label" htmlFor="student-id" fontSize="lg">
+          Student ID
+        </FormLabel>
+        <Input
+          id="student-id"
+          placeholder="Student ID"
+          size="lg"
+          {...register("studentId", {
+            required: "Student ID is required",
+            minLength: { value: 9, message: "Minimum length is 9" },
+            maxLength: { value: 14, message: "Maximum length is 14" },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.studentId && errors.studentId.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      {/* Course */}
+      <FormControl isInvalid={errors.course}>
+        <FormLabel id="course-label" htmlFor="course" fontSize="lg">
+          Course
+        </FormLabel>
+        <Select
+          id="course"
+          placeholder="Select course"
+          size="lg"
+          isRequired
+          {...register("course", {
+            required: "Course is required",
+          })}
+        >
+          {options?.map((data) => {
+            return (
+              <option key={data} value={data}>
+                {data}
+              </option>
+            );
+          })}
+        </Select>
+        <FormErrorMessage>
+          {errors.course && errors.course.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button
+        leftIcon={<CheckIcon />}
+        colorScheme="teal"
+        isLoading={isSubmitting}
+        type="submit"
+        mt={4}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+function getOptions() {
+  const options = [
+    "COP 1000C - Introduction to Programming Concepts",
+    "COP 2800C - Java Programming I",
+    "HUM 1020 - Introduction to Humanities",
+    "MAC 1105 - College Algebra",
+    "MAC 1140 - Precalculus Algebra",
+  ];
+
+  return options;
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,35 +1,18 @@
+import { Heading } from "@chakra-ui/react";
+import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
-import useSWR, { useSWRConfig } from "swr";
-import axios, { Method } from "axios";
-import { CreateTicket, Ticket } from "../types/ticket";
-import { Button, Heading } from "@chakra-ui/react";
-import { createTicket } from "../apis/tickets.api";
+import useSWR from "swr";
+import { CreateTicketForm } from "../components/forms/CreateTicketForm";
+import { Ticket } from "../types/ticket";
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-const TICKETS_URL = "http://localhost:3000/tickets";
 
 const IndexPage: NextPage = () => {
-  const { data } = useSWR<Ticket[]>(TICKETS_URL, fetcher);
-
-  const { mutate } = useSWRConfig();
-
-  const handleCreateTicket = async () => {
-    const newTicket: CreateTicket = {
-      studentId: "V000000000",
-      course: "MAC 1105 | College Algebra",
-    };
-
-    mutate(
-      TICKETS_URL,
-      [...(data || []), { id: "key", ...newTicket }], // Let key be id placeholder until revalidation
-      false
-    );
-
-    await createTicket(TICKETS_URL, newTicket);
-
-    mutate(TICKETS_URL);
-  };
+  const { data, mutate } = useSWR<Ticket[]>(
+    "http://localhost:3000/tickets",
+    fetcher
+  );
 
   return (
     <div>
@@ -40,17 +23,19 @@ const IndexPage: NextPage = () => {
       </Head>
 
       <main>
-        <Heading>Tickets</Heading>
-        <Button isDisabled={!data} onClick={handleCreateTicket}>
-          Create
-        </Button>
-        {data?.map((ticket) => {
-          return (
-            <div key={ticket.id}>
-              {ticket.studentId}, {ticket.course},{ticket.createDate}
-            </div>
-          );
-        })}
+        <Heading>Check-in.App</Heading>
+        {data
+          ?.slice(0)
+          .reverse()
+          .map((ticket) => {
+            return (
+              <div key={Math.random() * 1000}>
+                {ticket.studentId}, {ticket.course}
+              </div>
+            );
+          })}
+
+        <CreateTicketForm onMutate={mutate} />
       </main>
 
       <footer></footer>
